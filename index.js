bb49a295a20bf7b714a6be3f0780f9923782cec7
Danny Christanto <dnny.ch@gmail.com>
2017-03-09 12:08:41 +0700

    change interval

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const awake = require('keepawake')
 const config = {
   sleepTime: '17:00',       // '00:00' default
   wakeUpTime: '1:00',       // '06:00' default
-  keepaliveInterval: 30     // 5 minutes default
+  keepaliveInterval: 5     // 5 minutes default
 }
 
 const me = 'limitless-ridge-71279'
